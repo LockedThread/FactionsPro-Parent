65801b42e839d421a176e47287100d6a7d4d2bfd
LockedThread <proteinshakedev@gmail.com>
2019-09-03 00:18:54 -0400

    Fixed full command string
    Took 3 minutes

--- FactionsPro/src/main/java/dev/lockedthread/factionspro/commands/executor/FCommandExecutor.java
+++ FactionsPro/src/main/java/dev/lockedthread/factionspro/commands/executor/FCommandExecutor.java
@@ -32,7 +32,7 @@ public class FCommandExecutor implements CommandExecutor {
         String realLabel = colonIndex != -1 ? label.substring(colonIndex + 1) : label;
         FCommand fCommand = fCommandMap.get(realLabel);
         if (fCommand != null) {
-            return fCommand.perform(new ImmutableCommandContext(sender, "/" + label + " " + Joiner.on(" ").skipNulls().join(args), args, realLabel));
+            return fCommand.perform(new ImmutableCommandContext(sender, "/" + realLabel + " " + Joiner.on(" ").skipNulls().join(args), args, realLabel));
         }
         throw new RuntimeException("Unable to find registered FCommand even though it's registered to this executor");
     }
