ce102072ee7cc030cb39901cb2798354aa8b70d1
LockedThread <proteinshakedev@gmail.com>
2019-09-03 14:03:56 -0400

    Created FactionMessages enum
    Took 52 minutes

---
+++ FactionsPro/src/main/java/dev/lockedthread/factionspro/messages/FactionsMessages.java
@@ -0,0 +1,34 @@
+package dev.lockedthread.factionspro.messages;
+
+import org.bukkit.ChatColor;
+
+public enum FactionsMessages implements IMessages {
+
+    COMMAND_UNABLE_TO_PARSE_ARGUMENT("&cUnable to parse {argument} as {type}");
+
+    private String unformattedMessage;
+
+    FactionsMessages(String unformattedMessage) {
+        this.unformattedMessage = unformattedMessage;
+    }
+
+    @Override
+    public String getConfigKey() {
+        return name().toLowerCase().replace("_", "-");
+    }
+
+    @Override
+    public String getMessage() {
+        return ChatColor.translateAlternateColorCodes('&', unformattedMessage);
+    }
+
+    @Override
+    public void setMessage(String message) {
+        this.unformattedMessage = message;
+    }
+
+    @Override
+    public String getUnformattedMessage() {
+        return unformattedMessage;
+    }
+}
