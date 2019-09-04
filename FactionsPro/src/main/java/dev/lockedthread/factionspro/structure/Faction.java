b867ec39c9e3d6054a48fb3603b1aac70078d507
LockedThread <proteinshakedev@gmail.com>
2019-09-04 16:16:45 -0400

    Edited Faction class to build a proper layout
    Took 41 seconds

--- FactionsPro/src/main/java/dev/lockedthread/factionspro/structure/Faction.java
+++ FactionsPro/src/main/java/dev/lockedthread/factionspro/structure/Faction.java
@@ -1,22 +1,48 @@
 package dev.lockedthread.factionspro.structure;
 
+import dev.lockedthread.factionspro.structure.enums.Role;
 import dev.lockedthread.factionspro.structure.position.ChunkPosition;
 import lombok.Data;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 @Data
 public class Faction {
 
 
-    private int id;
+    private final int id;
 
-    private Set<ChunkPosition> chunkPositionSet;
-    private transient Set<FactionPlayer> factionPlayerSet;
+    private transient final Set<FactionPlayer> factionPlayerSet;
+
+    private transient FactionPlayer leader;
+    private transient Set<ChunkPosition> chunkPositionSet;
+
     private String name;
 
-    public Faction(int id) {
-
+    public Faction(int id, String name, FactionPlayer leader) {
+        this.id = id;
+        this.name = name;
+        this.leader = leader;
+        (this.factionPlayerSet = new HashSet<>(1)).add(leader);
     }
+
+    public Set<FactionPlayer> getFactionPlayers(Role role) {
+        if (factionPlayerSet.size() == 1) {
+            return role == Role.LEADER ? factionPlayerSet : Collections.emptySet();
+        }
+        Set<FactionPlayer> set = null;
+        for (FactionPlayer factionPlayer : factionPlayerSet) {
+            if (factionPlayer.getRole() == role) {
+                if (set == null) {
+                    set = new HashSet<>();
+                }
+                set.add(factionPlayer);
+            }
+        }
+        return set == null ? Collections.emptySet() : set;
+    }
+
 
 }
