d35314afc6cc859892efe72026af814b4b284a13
LockedThread <proteinshakedev@gmail.com>
2019-09-04 16:17:24 -0400

    Worked on the power system for FactionPlayers, created constructor, and set uuid to final
    Took 39 seconds

--- FactionsPro/src/main/java/dev/lockedthread/factionspro/structure/FactionPlayer.java
+++ FactionsPro/src/main/java/dev/lockedthread/factionspro/structure/FactionPlayer.java
@@ -1,5 +1,6 @@
 package dev.lockedthread.factionspro.structure;
 
+import dev.lockedthread.factionspro.configs.FactionsConfig;
 import dev.lockedthread.factionspro.structure.enums.Role;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,17 +18,44 @@ public class FactionPlayer {
 
     private transient Faction faction;
     private transient Player player;
-    private UUID uuid;
+    private final UUID uuid;
     private double power;
+    private double maxPower;
     private Role role;
 
+    public FactionPlayer(UUID uuid, Role role) {
+        this.uuid = uuid;
+        this.role = role;
+
+        /* Power */
+        this.maxPower = FactionsConfig.players_power_maximum;
+        if (FactionsConfig.players_power_startWithNone) {
+            this.power = 0.0;
+        } else {
+            this.power = FactionsConfig.players_power_starting;
+        }
+    }
+
+    public void die() {
+        if (FactionsConfig.players_power_death_loose) {
+            double futureAmount = power - FactionsConfig.players_power_death_amount;
+            if (FactionsConfig.players_power_allowNegative) {
+                this.power = futureAmount;
+            } else if (futureAmount < FactionsConfig.players_power_minimum) {
+                this.power = FactionsConfig.players_power_minimum;
+            } else {
+                this.power = futureAmount;
+            }
+        }
+    }
 
     @Nullable
     public Player getPlayer() {
         return player;
     }
 
-    public @NotNull OfflinePlayer getOfflinePlayer() {
+    @NotNull
+    public OfflinePlayer getOfflinePlayer() {
         return player == null ? player = (Player) Bukkit.getOfflinePlayer(uuid) : player;
     }
 
